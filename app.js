74d6dc2306af98b7678480031609b25c267922f1
javoxhr <javohiruzum@gmail.com>
2025-03-21 22:19:39 +0500

    new

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ tg.expand()
 tg.MainButton.setText("Отправить")
 tg.MainButton.show()
 
+const InpTitle = document.querySelector('#title').value
+const InpDesc = document.querySelector('#desc').value
+const InpStar = document.querySelector('#star').value
+const category = document.querySelector('#categorys').value
+
+let data = {
+    title: InpTitle,
+    desc: InpDesc,
+    stars: InpStar,
+    category: category
+}
+
 tg.MainButton.onClick(()=> {
-    alert("Кнопка нажата!")
+    tg.sendData(JSON.stringify(data))
 })
