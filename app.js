e706cd392ea52a6e86531feec57683af24a24fd8
javoxhr <javohiruzum@gmail.com>
2025-03-21 23:07:42 +0500

    new

--- app.js
+++ app.js
@@ -20,28 +20,5 @@ let data = {
 }
 
 tg.MainButton.onClick(() => {
-    if (InpTitle.length == 0) {
-        InpTitle.style.border = "2px solid red"
-        labelTitle.style.color = "red"
-        setTimeout(() => {
-            InpTitle.style.border = "2px solid var(--tg-theme-text-color)"
-            labelTitle.style.color = "var(--tg-theme-text-color)"
-        }, 2000)
-    } else if (InpDesc.length == 0) {
-        InpDesc.style.border = "2px solid red"
-        labelDesc.style.color = "red"
-        setTimeout(() => {
-            InpDesc.style.border = "2px solid var(--tg-theme-text-color)"
-            labelDesc.style.color = "var(--tg-theme-text-color)"
-        }, 2000)
-    } else if (InpStar.length == 0) {
-        InpStar.style.border = "2px solid red"
-        InpStar.style.color = "red"
-        setTimeout(() => {
-            InpStar.style.border = "2px solid var(--tg-theme-text-color)"
-            InpStar.style.color = "var(--tg-theme-text-color)"
-        }, 2000)
-    } else {
-        tg.sendData(JSON.stringify(data))
-    }
+    tg.sendData(JSON.stringify(data))
 })
