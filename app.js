a4caecbe883475c7175be37d3ff301ee62477f1b
javoxhr <javohiruzum@gmail.com>
2025-03-21 22:47:20 +0500

    new

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const InpDesc = document.querySelector('#desc').value
 const InpStar = document.querySelector('#star').value
 const category = document.querySelector('#categorys').value
 
+const labelTitle = document.querySelector('.title-label')
+
 let data = {
     title: InpTitle.value,
     desc: InpDesc,
@@ -20,8 +22,10 @@ let data = {
 tg.MainButton.onClick(() => {
     if (!InpTitle.length) {
         InpTitle.style.border = "2px solid red"
+        labelTitle.style.color = "red"
         setTimeout(() => {
             InpTitle.style.border = "2px solid var(--tg-theme-text-color)"
+            labelTitle.style.color = "var(--tg-theme-text-color)"
         }, 2000)
     } else {
         tg.sendData(JSON.stringify(data))
